1949a9df5a57d33da7ba3ab6118ab4864dedb4ff
mjterrs20 <aditia20.riz@gmail.com>
2019-10-23 08:38:29 +0700

    firstcommit

---
+++ app/src/main/java/com/example/hajicerdas/Handler.java
@@ -0,0 +1,6 @@
+package com.example.hajicerdas;
+
+class Handler{
+    public void postDelayed(Runnable runnable , int lengthLong) {
+    }
+}
---
+++ app/src/main/java/model/User.java
@@ -0,0 +1,16 @@
+package model;
+
+public class User {
+    public String id;
+    public String userName;
+    public String noPorsi;
+    public String password;
+
+    public User(String id, String userName, String noPorsi, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.noPorsi = noPorsi;
+        this.password = password;
+    }
+
+}
